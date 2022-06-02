fb7f691151502901ac86620c85532ab43160f76f
Breno Lemos <brenomiros@gmail.com>
2022-06-02 20:53:56 -0300

    Add project

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title>React Avançado</S.Title>
+    <S.Description>
+      TypeScript, React, NextJS and Styled Components
+    </S.Description>
+  </S.Wrapper>
+)
+
+export default Main
